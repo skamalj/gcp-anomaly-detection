0a2c840191bbc32f34419c22b12bb23d064093df
kamal <kamal>
2022-02-20 18:26:40 +0530

    Initial commit

--- process.py
+++ process.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def run(input_sub, timer, pipeline_args=None):
+def run(input_sub, model_load_timer, pipeline_args=None):
     import random
     # Set `save_main_session` to True so DoFns can access globally imported modules.
 
@@ -56,7 +56,7 @@ def run(input_sub, timer, pipeline_args=None):
 
     first_timestamp = Timestamp.now()
     model_trigger = (stream_pipeline 
-                | 'PeriodicImpulse' >> PeriodicImpulse(first_timestamp,MAX_TIMESTAMP, 120, False))
+                | 'PeriodicImpulse' >> PeriodicImpulse(first_timestamp,MAX_TIMESTAMP, model_load_timer, False))
     model_input = model_trigger | "Load Saved Models" >> LoadModels(project_id=project_id)
 
     
@@ -66,17 +66,18 @@ def run(input_sub, timer, pipeline_args=None):
     is_anomaly = norm_distances | "Filter Anomalies" >> beam.Filter(lambda x: x[1] > 3.0)
     is_anomaly | "Log Anomalies" >> ParDo(LogEventsWithTimestampAndWindow("Anomaly Detected: "))
 
-    (is_anomaly
-        | "Save Alerts to YSQL" >> ParDo(WriteToYSQL(
-                table="alerts",
-                database="yugabyte",
-                project_id=project_id,
-                port=5433,
-                user_secret_name="cql_user",
-                password_secret_name="cql_password",
-                host_secret_name="cql_host",
-                ca_cert_secret_name="cql_ca_cert"
-            )))   
+    # TODO
+    #(is_anomaly
+    #    | "Save Alerts to YSQL" >> ParDo(WriteToYSQL(
+    #            table="alerts",
+    #            database="yugabyte",
+    #            project_id=project_id,
+    #            port=5433,
+    #            user_secret_name="cql_user",
+    #            password_secret_name="cql_password",
+    #            host_secret_name="cql_host",
+    #            ca_cert_secret_name="cql_ca_cert"
+    #        )))   
 
     stream_pipeline.run()
 
@@ -90,13 +91,13 @@ if __name__ == "__main__":
         help="The Cloud Pub/Sub subscription to read from.", default="sourcesub"
     )
     parser.add_argument(
-        "--timer_timeout",
-        help="Timeout in seconds. Device will be marked as Failed when no data is recieved for this duration", default=30
+        "--model_load_timer",
+        help="TPulse time for model loads", default=120
     )
     known_args, pipeline_args = parser.parse_known_args()
 
     run(
         known_args.input_sub,
-        known_args.timer_timeout,
+        known_args.model_load_timer,
         pipeline_args,
     )
